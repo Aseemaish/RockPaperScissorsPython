a9333aa5583b4fbd9833abe5869db6e3e73d527b
Aseemaish <89722218+Aseemaish@users.noreply.github.com>
2024-06-22 22:34:40 +0530

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,5 +1,5 @@
-import random
-rock = '''
+import random    #Importing the random module and then using ASCII art images for better game experience.
+rock = '''       
     _______
 ---'   ____)
       (_____)
@@ -23,8 +23,8 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-ans = int(input("What do you choose? Type 0 for rock, 1 for paper, or 2 for scissors: "))
-lst = [rock, paper, scissors]
+ans = int(input("What do you choose? Type 0 for rock, 1 for paper, or 2 for scissors: "))    #User input 
+lst = [rock, paper, scissors]                                                                #List made so as to implement randomization.
 
 if ans == 0:
     print(rock)
@@ -33,13 +33,13 @@ elif ans == 1:
 elif ans == 2:
     print(scissors)
 else:
-    print("Invalid input.")
+    print("Invalid input.")                                                                  #Checking the invalid condition here only in order to avoid hoch poch later on.
     exit()
 comp = random.randint(0, 2)
 print(f"Computer chose:\n{lst[comp]}")
-if(ans == 0 and comp == 2) or (ans == 1 and comp == 0) or (ans == 2 and comp == 1):
+if(ans == 0 and comp == 2) or (ans == 1 and comp == 0) or (ans == 2 and comp == 1):           #Chwcking the conditions for user's win.
     print("You win!")
 elif ans == comp:
     print("It's a draw.")
-else:
+else:                                                                                         #All the other conditions except user's win and draw.
     print("You lose.")
